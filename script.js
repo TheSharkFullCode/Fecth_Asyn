099d23a61758f3327b9d13b21261cb2ddcb91221
TheSharkFullCode <agedjoscar@hotmail.com>
2023-08-04 00:09:03 +0200

    done

--- script.js
+++ script.js
@@ -114,9 +114,13 @@ getLuke()
 // -----------------Aqui no lo encontre  luke---------------------------------------------
 async function getMovies() {
     try {
+
       const response = await fetch('https://swapi.dev/api/people/1/');
+
       const luke = await response.json();
+
       const movies = luke.films.map(film => film.title);
+
       console.log(movies);
     } catch (error) {
       console.error(error);
@@ -126,6 +130,14 @@ async function getMovies() {
   getMovies();
 
 // -------------------------------------------------------------------------------
+
+
+
+
+
+
+
+
 async function getMoviesOfLukeSkywalker() {
 
 
@@ -167,7 +179,16 @@ async function getMoviesOfLukeSkywalker() {
 
 
 
-// Busca el personaje de Leia Organa y consigue que se muestre por consola la información del planet aen el que vivie: Alderaan.....................[este no va.. nose porque ]
+
+
+
+
+
+
+
+// Busca el personaje de Leia Organa y consigue que se muestre por consola la información del planet aen el que vivie: Alderaan....
+// .................[este no va.. nose porque ]
+
 async function LeiaOrgan() {
     try {
       const leiA = await fetch('https://swapi.dev/api/people/5/');
@@ -237,12 +258,161 @@ function LeiaOrgan() {
 
 
 
+async function apicafeAltura(){
+
+    try{
+        const response = await fetch('https://cafe-de-altura.vercel.app/api/products');
+        const data = await response.json();
+        console.log(data)
+
+    }catch(error){
+        console.log(error);
 
 
+    }
+}
+apicafeAltura()
+
+// -----------------------------------------------------------------------------
+const getFilmsLuke = async () => {
+  try {
+    const result = await fetch('https://swapi.dev/api/people');
+    const data = await result.json();
+
+    const characterLuke = data.results.find(character => character.name === "Luke Skywalker");
+
+    const lukeFilmsUrls = characterLuke.films;
+    const lukeFilmsData = await Promise.all(lukeFilmsUrls.map(async url => {
+      const response = await fetch(url);
+      const filmData = await response.json();
+      return filmData;
+    }));
+
+    lukeFilmsData.forEach(filmData => {
+      console.log(`Título: ${filmData.title}`);
+      console.log(`Episodio: ${filmData.episode_id}`);
+      console.log(`Director: ${filmData.director}`);
+      console.log("---------------------");
+    });
+
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+getFilmsLuke();
+
+// En este código, se utiliza la sintaxis de 
+// async/await
+//  para simplificar el código y hacerlo más legible. La función 
+// getFilmsLuke
+//  utiliza la función  fetch
+//  para obtener los datos de la API de la película. Luego, utiliza la función 
+// find  para encontrar los datos del personaje de Luke Skywalker en los resultados de la búsqueda.
+
+// Una vez que se encuentra los datos del personaje de Luke Skywalker, se obtienen los URLs de los episodios en los que aparece. Luego, se utiliza la función 
+// Promise.all  para esperar a que se completen todas las solicitudes HTTP antes de continuar con la ejecución del código.
+
+// Finalmente, se utiliza un bucle 
+// forEach  para recorrer los datos de los episodios y mostrar información sobre cada uno en la consola del navegador.
+
+// En resumen, este código utiliza la API de la película Star Wars: The Force Awakens para obtener información sobre los episodios de la película en los que aparece a Luke Skywalker y luego muestra esta información en la consola del navegador.
+
+// Espero que esto te haya sido útil. Si tienes alguna otra pregunta, no dudes en preguntar.
+
+// ----------------------------------------------------------------------------------------
 
 
+const getPlanetLeia = () => {
+  fetch('https://swapi.dev/api/people')
+    .then(result => result.json())
+    .then(data => {
+      const characterLeia = data.results.find(character => character.name === "Leia Organa");
+      const planetUrl = characterLeia.homeworld;
+      return fetch(planetUrl);
+    })
+    .then(leiaPlanet => leiaPlanet.json())
+    .then(leiaPlanetData => {
+      console.log('Leia es del planeta:', leiaPlanetData.name);
+      console.log('Con un diámetro de:', leiaPlanetData.diameter);
+      console.log('Y con un clima de:', leiaPlanetData.climate);
+    })
+    .catch(error => {
+      console.log(error);
+    });
+}
+ getPlanetLeia();
+// -------------------------------------------------------------------------
+//  -------------------------------------------------------------------------
+const getPlanetLeiA = async () =>{
+  const result = await fetch('https://swapi.dev/api/people')
+  const data = await result.json()
+
+  const characterLeia = data.results.find(character => character.name === "Leia Organa")
+
+  const planetUrl = characterLeia.homeworld
+  const leiaPlanet = await fetch(planetUrl)
+
+  const leiaPlanetData =await leiaPlanet.json()
+  console.log('Leia es del planeta:', leiaPlanetData.name);
+  console.log('Con un diametro de:', leiaPlanetData.diameter);
+  console.log('Y con un clima de:', leiaPlanetData.climate);
+   
+}
+
+// En este código, se utiliza la sintaxis de 
+// async/await
+//  para simplificar el código y hacerlo más legible. La función 
+// getPlanetLeia
+//  utiliza la función  fetch
+//  para obtener los datos de la API de la película. Luego, utiliza la función 
+// find  para encontrar los datos del personaje de Leia Organa en los resultados de la búsqueda.
+
+// Una vez que se encuentra los datos del personaje de Leia Organa, se obtiene el URL del planeta donde vive. Luego, se utiliza la función  fetch
+//  para obtener los datos del planeta utilizando el URL obtenido.
+
+// Finalmente, se utiliza la función 
+// json  para convertir los datos del planeta en un objeto JavaScript y se muestra información sobre el planeta en la consola del navegador.
+
+// En resumen, este código utiliza la API de la película Star Wars: The Force Awakens para obtener información sobre la planeta donde vive y es posible que Leia Organa y luego muestra esta información en la consola del navegador.
+
+// ---------------------------------------------------------------------------------------------
+async function fetchCharacterData() {
+    
+      
+  const charactersResponse = await fetch('https://swapi.dev/api/people/');
+  const charactersData = await charactersResponse.json();
+  
+  
+  const c3poData = charactersData.results.find(character => character.name === 'C-3PO');
+  const c3poFilmsUrls = c3poData.films;
+
+  
+  const c3poFilmsData = await Promise.all(c3poFilmsUrls.map(url => fetch(url).then(response => response.json())));
 
 
+  c3poFilmsData.forEach(filmData => {
+    console.log(`Título: ${filmData.title}`);
+    console.log(`Episodio:, ${filmData.episode_id}`);
+    console.log(`Director:, ${filmData.director}`);
+    console.log("---------------------");
+  });
+  }
+  fetchCharacterData()
 
+  // Este código utiliza la API de la película Star Wars: The Force Awakens para obtener información sobre los episodios de la película en los que aparece a C-3PO.
+
+  // La función  fetchCharacterData  utiliza la función 
+  // fetch
+  //  para obtener los datos de la API de la película. Luego, utiliza la función  find
+  //  para encontrar los datos del personaje de C-3PO en los resultados de la búsqueda.
+  
+  // Una vez que se encuentra los datos del personaje de C-3PO, se obtienen los URLs de los episodios en los que aparece. Luego, se utiliza la función 
+  // Promise.all  para esperar a que se completen todas las solicitudes HTTP antes de continuar con la ejecución del código.
+  
+  // Finalmente, se utiliza un bucle  forEach
+  //  para recorrer los datos de los episodios y mostrar información sobre cada uno en la consola del navegador.
+  
+  // En resumen, este código utiliza la API de la película Star Wars: The Force Awakens para obtener información sobre los episodios de la película en los que aparece a C-3PO y luego muestra esta información en la consola del navegador
 
 
